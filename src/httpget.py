b81d87c218674e85581616acfdf97c91a9e54fa4
Bernhard Bruns <bernhard.bruns@googlemail.com>
2023-11-17 14:45:17 +0100

    Create httpget.py

---
+++ src/httpget.py
@@ -0,0 +1,24 @@
+import json
+
+import requests
+
+import models
+
+
+class HttpGet:
+   
+
+    def __init__(self, settings: models.Settings) -> None:
+        """Init."""
+        self.settings = settings
+
+    def press(self, action_params: models.HttpGet) -> None:
+       
+              
+        requests.get(action_params.path)
+        print(
+               'Try GET-Request for URL: ({action_params.path}) '
+            )
+     
+        
+        )
